96e426d3d8718d66bd1fc39e8a9550a04ec6510e
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-04-01 21:12:06 +0100

    files

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({port: 3333}).then(() => console.log("HTTP server running on port 3333!"))
+app
+  .listen({ port: 3333 })
+  .then(() => console.log('HTTP server running on port 3333!'))
