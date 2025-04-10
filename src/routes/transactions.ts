a11437dcd7f56e97262db4cccbca5b77adc8ea66
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-04-10 22:01:04 +0100

    feat: separando rotas da aplicação em uma pasta routes - Validação do body com zod para melhor tipagem - Desestruturamos diretamente para maior facilidade - Adicionado no banco de dados e como não inserimos um campo type na tabela, fazemos a verificação se é credito o valor é positivo, se for debito o valor é tratado como diminutivo fazendo * -1 e facilitando a soma la na frente, visto que somando positivos e negativos o valor é o mesmo

---
+++ src/routes/transactions.ts
@@ -0,0 +1,26 @@
+import type { FastifyInstance } from 'fastify'
+import { knex } from '../db'
+import z from 'zod'
+import { randomUUID } from 'crypto'
+
+export async function TransactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['debit', 'credit']),
+    })
+
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
