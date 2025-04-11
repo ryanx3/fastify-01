9ed7ef054417f0093401754c6fc633c2b5104506
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-04-11 23:16:23 +0100

    O app.addHook('preHandler', SessionIdExists) só vai afetar as rotas que forem definidas depois dele. Ou seja, podemos definir as rotas que não queremos esse hook acima dele ou passar o preHandler direto para rotas que vão usar outros hooks. a ordem em que você chama app.addHook e define as rotas importa. Se você registrar o hook depois de já ter definido as rotas, ele não será aplicado retroativamente.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,45 +5,6 @@ import { randomUUID } from 'crypto'
 import { SessionIdExists } from '../middlewares/check-if-session-id-exists'
 
 export async function TransactionsRoutes(app: FastifyInstance) {
-  app.get('/', { preHandler: [SessionIdExists] }, async (request) => {
-    const { sessionId } = request.cookies
-    const allTransactions = await knex('transactions')
-      .select('*')
-      .where('session_id', sessionId)
-    return {
-      allTransactions,
-    }
-  })
-
-  app.get('/:id', { preHandler: [SessionIdExists] }, async (request) => {
-    const getTransactionByIdParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
-    const { sessionId } = request.cookies
-    const { id } = getTransactionByIdParamsSchema.parse(request.params)
-
-    const transaction = await knex('transactions')
-      .where({
-        session_id: sessionId,
-        id,
-      })
-      .first()
-
-    return {
-      transaction,
-    }
-  })
-
-  app.get('/summary', { preHandler: [SessionIdExists] }, async (request) => {
-    const { sessionId } = request.cookies
-    const summary = await knex('transactions')
-      .where('session_id', sessionId)
-      .sum('amount', { as: 'amount_total' })
-      .first()
-
-    return { summary }
-  })
-
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -74,5 +35,46 @@ export async function TransactionsRoutes(app: FastifyInstance) {
     })
 
     return reply.status(201).send()
+  })
+
+  app.addHook('preHandler', SessionIdExists)
+
+  app.get('/', async (request) => {
+    const { sessionId } = request.cookies
+    const allTransactions = await knex('transactions')
+      .select('*')
+      .where('session_id', sessionId)
+    return {
+      allTransactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionByIdParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { sessionId } = request.cookies
+    const { id } = getTransactionByIdParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async (request) => {
+    const { sessionId } = request.cookies
+    const summary = await knex('transactions')
+      .where('session_id', sessionId)
+      .sum('amount', { as: 'amount_total' })
+      .first()
+
+    return { summary }
   })
 }
