fd8af0cd525010ed74a937027aebfde89eaba7d5
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-04-11 22:52:43 +0100

    feat: registrando cookie no app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { TransactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(TransactionsRoutes, {
   prefix: 'transactions',
 })
