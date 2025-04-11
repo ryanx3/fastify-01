7937d2d0c645e3dce1218471f4f6111feee2b7b1
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-04-11 23:14:07 +0100

    fix: retirando, pois era apenas exemplo para anotar no git e não esquecer

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,10 @@ import fastify from 'fastify'
 import { env } from './env'
 import { TransactionsRoutes } from './routes/transactions'
 import cookie from '@fastify/cookie'
-import { SessionIdExists } from './middlewares/check-if-session-id-exists'
 
 const app = fastify()
 
 app.register(cookie)
-app.addHook('preHandler', SessionIdExists)
 app.register(TransactionsRoutes, {
   prefix: 'transactions',
 })
